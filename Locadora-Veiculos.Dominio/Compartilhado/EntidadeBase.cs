1314b82208038b97a7690f74de7ee30257f1fd65
Matheus <Matheus@MATHEUS>
2022-06-22 11:13:20 -0300

    Add project files.

---
+++ Locadora-Veiculos.Dominio/Compartilhado/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora_Veiculos.Dominio.Compartilhado
+{
+    public abstract class EntidadeBase<T>
+    {
+        public int Id { get; set; }
+    }
+}
