ce0e09961fefa2f751d28c242dea6bc696ed4dfa
Matheus <Matheus@MATHEUS>
2022-06-22 11:55:10 -0300

    Adicionando as tabelas de banco de dados.

---
+++ Locadora-Veiculos.Projeto.SqlServer/dbo/Tables/TBTaxa.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[TBTaxa] (
+    [Id]        INT           IDENTITY (1, 1) NOT NULL,
+    [Descricao] VARCHAR (300) NOT NULL,
+    [Valor]     DECIMAL (18)  NOT NULL,
+    CONSTRAINT [PK_TBTaxa] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
