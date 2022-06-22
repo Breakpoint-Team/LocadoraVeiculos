63665bde10252f0689aa51bcea9a5e3af8240d61
Matheus <Matheus@MATHEUS>
2022-06-22 16:19:19 -0300

    Atualizando a estrutura do banco de dados.

--- Locadora-Veiculos.Projeto.SqlServer/dbo/Tables/TBCliente.sql
+++ Locadora-Veiculos.Projeto.SqlServer/dbo/Tables/TBCliente.sql
@@ -1,13 +1,12 @@
 ﻿CREATE TABLE [dbo].[TBCliente] (
     [Id]           INT           IDENTITY (1, 1) NOT NULL,
     [Nome]         VARCHAR (300) NOT NULL,
-    [Cpf]          VARCHAR (300) NULL,
-    [Cnpj]         VARCHAR (300) NULL,
+    [Documento]    VARCHAR (300) NOT NULL,
     [Email]        VARCHAR (300) NOT NULL,
     [Telefone]     VARCHAR (300) NOT NULL,
     [Endereco]     VARCHAR (300) NOT NULL,
     [Tipo_cliente] VARCHAR (300) NOT NULL,
-    [Cnh]          VARCHAR (300) NULL,
+    [Cnh]          VARCHAR (300) NOT NULL,
     CONSTRAINT [PK_TBCliente] PRIMARY KEY CLUSTERED ([Id] ASC)
 );
 
