ddfd64c7fba6e5c9ed33ab082d559a97983bce18
Camila Valdrigues Candido <camilacandidov@gmail.com>
2022-06-23 18:53:21 -0300

    Implementado teste unitário e de integração do modulo de taxa

--- Locadora-Veiculos.Dominio/ModuloTaxa/ValidadorTaxa.cs
+++ Locadora-Veiculos.Dominio/ModuloTaxa/ValidadorTaxa.cs
@@ -5,5 +5,19 @@ namespace Locadora_Veiculos.Dominio.ModuloTaxa
 {
     public class ValidadorTaxa : AbstractValidator<Taxa>
     {
+        public ValidadorTaxa()
+        {
+            RuleFor(x => x.Descricao)
+                .NotEmpty().WithMessage("O campo 'Descrição' é obrigatório!")
+                .NotNull().WithMessage("O campo 'Descrição' é obrigatório!");
+
+            RuleFor(x => x.Valor)
+                .NotEmpty().WithMessage("O campo 'Valor' é obrigatório!")
+                .NotNull().WithMessage("O campo 'Valor' é obrigatório!");
+
+            //RuleFor(x => x.TipoCalculo)
+            //     .NotEmpty().WithMessage("O campo 'Tipo de Cálculo' é obrigatório!")
+            //    .NotNull().WithMessage("O campo 'Tipo de Cálculo' é obrigatório!");
+        }
     }
 }
---
+++ Locadora-Veiculos.Infra.BancoDados/ModuloTaxa/RepositorioTaxaEmBancoDados.cs
@@ -0,0 +1,56 @@
+﻿
+
+using Locadora_Veiculos.Dominio.ModuloTaxa;
+using Locadora_Veiculos.Infra.BancoDados.Compartilhado;
+
+namespace Locadora_Veiculos.Infra.BancoDados.ModuloTaxa
+{
+    public class RepositorioTaxaEmBancoDados : RepositorioBase<Taxa, ValidadorTaxa, MapeadorTaxa>, IRepositorioTaxa
+    {
+        protected override string sqlInserir =>
+          @"INSERT INTO [TBTAXA]
+                (
+                    [DESCRICAO],
+                    [VALOR],
+                    [TIPOCALCULO]
+                )
+            VALUES
+                (
+                    @DESCRICAO,
+                    @VALOR,
+                    @TIPOCALCULO
+                ); SELECT SCOPE_IDENTITY();";
+
+        protected override string sqlEditar =>
+             @" UPDATE [TBTAXA]
+                    SET 
+                        [DESCRICAO] = @DESCRICAO,
+                        [VALOR] = @VALOR,
+                        [TIPOCALCULO] = @TIPOCALCULO
+                    WHERE [ID] = @ID";
+
+        protected override string sqlExcluir =>
+            @"DELETE FROM [TBTAXA]
+                WHERE [ID] = @ID";
+
+        protected override string sqlSelecionarPorId =>
+         @"SELECT 
+                [ID],       
+                [DESCRICAO],
+                [VALOR],
+                [TIPOCALCULO]
+            FROM
+                [TBTAXA]
+            WHERE 
+             [ID] = @ID";
+
+        protected override string sqlSelecionarTodos =>
+        @"SELECT 
+                [ID],       
+                [DESCRICAO],
+                [VALOR],
+                [TIPOCALCULO]
+            FROM
+                [TBTAXA]";
+    }
+}
