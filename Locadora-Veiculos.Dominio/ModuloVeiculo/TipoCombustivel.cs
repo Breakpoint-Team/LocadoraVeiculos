1339bc5e55a2b2ee9a100fc2af8d13445ac9c8cf
Camila Valdrigues Candido <camilacandidov@gmail.com>
2022-07-01 15:30:48 -0300

    feat: Implementação do domíno do modulo de veículo

---
+++ Locadora-Veiculos.Dominio/ModuloVeiculo/TipoCombustivel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Locadora_Veiculos.Dominio.ModuloVeiculo
+{
+    public enum TipoCombustivel
+    {
+        [Description("Gasolina")]
+        Gasolina,
+
+        [Description("Diesel")]
+        Diesel,
+
+        [Description("GNV")]
+        GNV,
+
+        [Description("Álcool")]
+        Alcool,
+
+        [Description("Flex")]
+        Flex
+
+
+    }
+}
---
+++ Locadora-Veiculos.Dominio/ModuloVeiculo/Veiculo.cs
@@ -0,0 +1,46 @@
+﻿using Locadora_Veiculos.Dominio.Compartilhado;
+using Locadora_Veiculos.Dominio.ModuloGrupoVeiculos;
+
+namespace Locadora_Veiculos.Dominio.ModuloVeiculo
+{
+    public class Veiculo : EntidadeBase<Veiculo>
+    {
+        public Veiculo()
+        {
+
+        }
+
+        public Veiculo(string modelo, string marca, int ano, string cor, string placa, TipoCombustivel tipoCombustivel, int quilometragemPercorrida, int capacidadeTanque, GrupoVeiculos grupoVeiculos)
+        {
+            Modelo = modelo;
+            Marca = marca;
+            Ano = ano;
+            Cor = cor;
+            Placa = placa;
+            TipoCombustivel = tipoCombustivel;
+            QuilometragemPercorrida = quilometragemPercorrida;
+            CapacidadeTanque = capacidadeTanque;
+            GrupoVeiculos = grupoVeiculos;
+        }
+
+        public string Modelo { get; set; }
+        public string Marca { get; set; }
+        public int Ano { get; set; }
+        public string Cor { get; set; }
+        public string Placa { get; set; }
+        public TipoCombustivel TipoCombustivel { get; set; }
+        public int QuilometragemPercorrida { get; set; }
+        public decimal CapacidadeTanque { get; set; }
+        public GrupoVeiculos GrupoVeiculos { get; set; }
+
+        //public byte[] Imagem { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0}, {1}, {2}, {3}, {4}, {5}, {6}, {7}, {8}", Modelo, Marca, Ano, Cor, Placa, TipoCombustivel.GetDescription(), QuilometragemPercorrida, CapacidadeTanque, GrupoVeiculos);
+        }
+
+
+
+    }
+}
