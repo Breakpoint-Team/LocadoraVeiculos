024059596345b2b963908fad4578654808d8cd13
Matheus <Matheus@MATHEUS>
2022-07-30 17:40:55 -0300

    feat: implementada a gestão de exclusão de registro de tabelas n pra n (Locação e taxa).

---
+++ Locadora-Veiculos.Infra.BancoDados.ORM/Migrations/20220730192531_addRestricaoExclusaoTabelaLocacaoTaxa.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Locadora_Veiculos.Infra.BancoDados.ORM.Migrations
+{
+    public partial class addRestricaoExclusaoTabelaLocacaoTaxa : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LocacaoTaxa_TBTaxa_TaxasSelecionadasId",
+                table: "LocacaoTaxa");
+
+            migrationBuilder.DropTable(
+                name: "TBPrecoCombustivel");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LocacaoTaxa_TBTaxa_TaxasSelecionadasId",
+                table: "LocacaoTaxa",
+                column: "TaxasSelecionadasId",
+                principalTable: "TBTaxa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_LocacaoTaxa_TBTaxa_TaxasSelecionadasId",
+                table: "LocacaoTaxa");
+
+            migrationBuilder.CreateTable(
+                name: "TBPrecoCombustivel",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    PrecoAlcool = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoDiesel = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoGNV = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    PrecoGasolina = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TBPrecoCombustivel", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_LocacaoTaxa_TBTaxa_TaxasSelecionadasId",
+                table: "LocacaoTaxa",
+                column: "TaxasSelecionadasId",
+                principalTable: "TBTaxa",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
